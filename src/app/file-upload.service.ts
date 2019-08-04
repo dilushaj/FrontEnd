0b5a0bc845a04e47499bfba99f7561ce25ddab2d
Chamalie <chamalie.15@cse.mrt.ac.lk>
2019-08-04 16:43:10 +0530

    Recommendation process

--- src/app/file-upload.service.ts
+++ src/app/file-upload.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpEventType, HttpRequest, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpRequest, HttpResponse, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 //import { Subject } from "rxjs";
 
@@ -14,32 +14,48 @@ export class FileUploadService {
 
   public postFile(fileToUpload: FormData) {
     
-    const DJANGO_SERVER = "/api/upload";
-    
-    
-
+    const SERVER_URL = "/api/upload";
     return this.httpClient. 
-    post<any>(DJANGO_SERVER, fileToUpload, {
+    post<any>(SERVER_URL, fileToUpload, {
       reportProgress: true,
       observe: 'events'
     })
     .pipe(map((event) => {
-
       switch (event.type) {
-
         case HttpEventType.UploadProgress:
           const progress = Math.round(100 * event.loaded / event.total);
-          return { status: 'progress', message: progress };
-
+          return { status: 'progress', message: progress, data:''};
         case HttpEventType.Response:
-          return event.body;
-
+          return { status: event.statusText, message:0, data:(event.body.data).substr(1)}; //Substring taken to remove the leading '/' in the response.
         default:
-          return `Unhandled event: ${event.type}`;
+          return { status: event.type, message:0, data:''};
       }
     })
     );
-
   }
+
+  public getRecommendation(path: string) {
+    
+    const SERVER_URL = "/api/recommend";
+    let params = new HttpParams().set("filepath",path);
+    return this.httpClient. 
+    get<any>(SERVER_URL, {
+      reportProgress: true,
+      observe: 'events',
+      params: params
+    })
+    .pipe(map((event) => {
+      switch (event.type) {
+        case HttpEventType.DownloadProgress:
+          const progress = Math.round(100 * event.loaded / event.total);
+          return { status: 'progress', message: progress, algorithms:'' };
+        case HttpEventType.Response:
+          return { status: event.statusText, message: 0, algorithms: event.body.algorithms };
+        default:
+          return { status: event.type, message: 0, algorithms: '' };
+      }
+    })
+    );
+  } 
 
 }
--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -11,23 +11,29 @@ export class FileUploadComponent implements OnInit {
   fileUploadService: FileUploadService;  
   fileToUpload: File;
   form: FormGroup;
-  uploadResponse : Object = { status: '', message: '', filePath: '' };
+  recommendResponse = { status: undefined, message: undefined, algorithms: undefined };
+  uploadResponse = { status: undefined, message: undefined, data: undefined };
 
+  //constructor 
   constructor(private formBuilder: FormBuilder, fileUploadService:FileUploadService) { 
     this.fileUploadService = fileUploadService;
+    
   }
 
   ngOnInit() {
     this.form = this.formBuilder.group({
       file: ['']
     });
+    var recommendResponse  = { status: '', message: 0, algorithms: undefined };
+    this.recommendResponse = recommendResponse;
+    var uploadResponse = { status: '', message: 0, data: undefined };
+    this.uploadResponse = uploadResponse;
   }
 
   //get the raw dataset
   handleFileInput(files: FileList) {
     if (files.length > 0) {
     this.fileToUpload = files[0];
-    //this.form.get('data').setValue(this.fileToUpload);
     }
   }
 
@@ -37,9 +43,21 @@ export class FileUploadComponent implements OnInit {
     formData.append('data', this.fileToUpload);
     this.fileUploadService.postFile(formData).subscribe(res => {
       this.uploadResponse = res;
-      }, error => {
-        console.log(error);
-      });
+      if(this.uploadResponse.status==="Created"){
+        this.recommend(this.uploadResponse.data)
+    }
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  //obtain recommended algorithhms for the uploaded dataset from server.
+  recommend(path: string) {
+    this.fileUploadService.getRecommendation(path).subscribe(res =>{
+      this.recommendResponse = res;
+    },error => {
+      console.log(error);
+    });
   }
 
 }
